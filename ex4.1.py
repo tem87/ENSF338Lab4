4ec61791df4f3446167d44bfa1473b97be90cf39
brandonnguyen3 <144299768+brandonnguyen3@users.noreply.github.com>
2024-02-16 15:34:55 -0700

    Add files via upload

---
+++ ex4.1.py
@@ -0,0 +1,34 @@
+#ENSF 338 Lab 4 Exercise 4.1
+
+'''
+Question 1:
+The best case scenario is when none of the elements in the list
+are greater than 5. If this is the case, then the inner loop
+is never executed and the function goes through once. Therefore 
+the best case complexity is O(n).
+
+The worst case scenario is when all the elements in the list are
+less than 5. In this case the loops are fully executed. So the 
+resulting complexity is O(n^2)
+
+In the average case, we expect some elements to be greater than 5
+and some elements to be less than 5. So we can expect the inner loop
+and the outer loop to be executed. Therefore the average case
+complexity is O(n^2)
+'''
+
+import timeit
+import matplotlib.pyplot as plt
+
+'''
+Question 2:
+In this case all the complexities will be linear. 
+The inner loop now iterates a fixed number of times (5 in this case), regardless of the length of the input list.
+'''
+
+def processdata(li):
+    for i in range(len(li)):        # O(n)
+        if li[i] > 5:               # O(1)
+            for j in range(5):      # Fixed number of iterations
+                li[i] *= 2          # O(1)
+
---
+++ ex4.2.py
@@ -0,0 +1,69 @@
+#Inefficient Code
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+
+#Efficient Code
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+import time
+import random
+import matplotlib.pyplot as plt
+
+# Function to generate a sorted array
+def generate_sorted_array(size):
+    return sorted(random.sample(range(size * 3), size))
+
+# Time measurement function
+def measure_time(search_func, arr, target):
+    start_time = time.time()
+    search_func(arr, target)
+    end_time = time.time()
+    return end_time - start_time
+
+# Perform measurements
+sizes = [1000, 2000, 3000, 4000, 5000]
+num_measurements = 100
+
+linear_search_times = []
+binary_search_times = []
+
+for size in sizes:
+    arr = generate_sorted_array(size)
+    target = random.randint(0, size * 3)
+
+    linear_times = []
+    binary_times = []
+
+    for _ in range(num_measurements):
+        linear_time = measure_time(linear_search, arr, target)
+        binary_time = measure_time(binary_search, arr, target)
+
+        linear_times.append(linear_time)
+        binary_times.append(binary_time)
+
+    linear_search_times.append(sum(linear_times) / num_measurements)
+    binary_search_times.append(sum(binary_times) / num_measurements)
+
+# Plotting
+plt.plot(sizes, linear_search_times, label="Linear Search")
+plt.plot(sizes, binary_search_times, label="Binary Search")
+plt.xlabel('Input Size')
+plt.ylabel('Time (s)')
+plt.title('Comparison of Linear Search and Binary Search')
+plt.legend()
+plt.show()
