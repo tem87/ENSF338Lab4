0f77c59acf51c7d22cef18d57d7e92ae629c1752
AminaSagheer <144405367+AminaSagheer@users.noreply.github.com>
2024-02-27 19:52:06 -0700

    Update ex7.py
    question 2 coding part

--- ex7.py
+++ ex7.py
@@ -1,1 +1,12 @@
 #ENSF 338 Exercise 7
+def reverse(self):
+    current = self.head
+    prev = None
+
+    while current is not None:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+
+    self.head = prev
